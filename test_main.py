cdce54b36da955745aea243ee2686aa747e033ff
Neil Tolentino <ntolentino4@academic.rrc.ca>
2026-01-19 11:42:57 -0600

    Updated the test_main.py and added more tests.

--- test_main.py
+++ test_main.py
@@ -12,3 +12,5 @@ def test_hello_prints_expected_text(capsys):
     assert captured.out.strip() == "Hello, DevOps learner!"
 def test_bye_prints_expected_text(capsys):
     bye()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Goodbye!"
